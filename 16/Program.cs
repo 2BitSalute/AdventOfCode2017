f0475a6d25f31e17f4823c5060adc68e9ea2a6b1
2BitSalute <2bitsalute@gmail.com>
2017-12-15 22:08:30 -0800

    Day 16

--- 16/Program.cs
+++ 16/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace _16
 {
@@ -6,7 +8,110 @@ namespace _16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = File.ReadAllText("input.txt");
+            int numPrograms = 16;
+
+            char[] line = new char[numPrograms];
+            for (int i = 0; i < numPrograms; i++)
+            {
+                line[i] = (char)('a' + i);
+            }
+
+            int counter = 0;
+            string origLine = string.Join("", line);
+            for (int i = 0; i < 1000000000 % 63; i++)
+            {
+                line = SolvePart1(line, input);
+                string lineString = string.Join("", line);
+
+                counter++;
+                if (lineString == origLine)
+                {
+                    break;
+                }
+            }
+            
+            Console.WriteLine(string.Join("", line) + " " + counter);
+        }
+
+        public static char[] SolvePart1(char[] line, string input)
+        {
+            string[] moves = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(var move in moves)
+            {
+                switch(move[0])
+                {
+                    case 's':
+                        int i = int.Parse(move.Substring(1));
+                        Spin(i, line);
+                        break;
+                    case 'x':
+                        string[] tokens = move.Substring(1).Split('/', StringSplitOptions.RemoveEmptyEntries);
+                        int x = int.Parse(tokens[0]);
+                        int y = int.Parse(tokens[1]);
+
+                        Exchange(x, y, line);
+
+                        break;
+
+                    case 'p':
+                        string[] pTokens = move.Substring(1).Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+                        Partner(pTokens[0][0], pTokens[1][0], line);
+                        break;
+                    
+                    default:
+                        throw new Exception();
+                }
+            }
+
+            return line;
+        }
+
+        public static void Partner(char c1, char c2, char[] line)
+        {
+            Exchange(IndexOf(c1, line), IndexOf(c2, line), line);
+        }
+
+        public static int IndexOf(char c, char[] line)
+        {
+            for(int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == c)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static void Exchange(int i, int j, char[] line)
+        {
+            char temp = line[i];
+            line[i] = line[j];
+            line[j] = temp;
+        }
+
+        public static void Spin(int i, char[] line)
+        {
+            char[] temp = new char[i];
+
+            for (int j = i - 1; j >= 0; j--)
+            {
+                temp[j] = line[line.Length - (i - j)];
+            }
+
+            for (int j = line.Length - 1; j >= i; j--)
+            {
+                line[j] = line[j - i];
+            }
+
+            for (int j = 0; j < temp.Length; j++)
+            {
+                line[j] = temp[j];
+            }
         }
     }
 }
